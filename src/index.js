9ad1965118655ab5aefb1898215c552496fa15c7
Luiz Claudio <luizcns18@gmail.com>
2018-11-03 17:50:43 -0300

    feat: Add Link mutations

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 let links = [
   {
     id: "link-0",
@@ -26,17 +13,45 @@ let links = [
   }
 ];
 
-// 2
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: () => links,
+    link: (root, args) => links.find(link => link.id === args.id)
+  },
+  // Link: {
+  //   id: root => root.id,
+  //   description: root => root.description,
+  //   url: root => root.url
+  // }
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (root, args) => {
+      let link = links.find(link => link.id === args.id);
+      updateLink = { ...link, ...args };
+      links = links.map(link => (link.id === args.id ? updateLink : link));
+      return updateLink;
+    },
+    deleteLink: (root, { id }) => {
+      let link = links.find(link => link.id === id);
+      links = links.filter(link => link.id !== id);
+      return link;
+    }
   }
 };
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
