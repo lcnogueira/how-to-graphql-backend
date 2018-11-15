6c78ad191c74b8b597a26fa7763d395eae276de8
Luiz Claudio <luizcns18@gmail.com>
2018-11-15 09:36:25 -0300

    feat: Add filtering, sorting and pagination

---
+++ src/resolvers/Feed.js
@@ -0,0 +1,12 @@
+function links(parent, args, context, info) {
+  return context.db.query.links(
+    {
+      where: { id_in: parent.links }
+    },
+    info
+  );
+}
+
+module.exports = {
+  links
+};
